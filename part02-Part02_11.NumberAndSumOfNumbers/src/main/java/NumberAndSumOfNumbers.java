9ce03a541b77a99ead13d94c41eb5a0982dc01be
superj4mes <jesse.stromberg@gmail.com>
2022-02-02 19:49:41 +0200

    Part2 1-13 completed

---
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class NumberAndSumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        int count = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            sum = sum + number;
+            count++;
+            
+        }
+
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + sum);
+        
+    }
+}
